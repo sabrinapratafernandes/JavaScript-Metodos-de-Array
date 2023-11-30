f3d9d92317f6aa04f1d69e5236eae59819951231
Sabrina <sabrinabuenoprata@gmail.com>
2023-11-30 15:48:00 -0300

    adicionando mais filtros

--- app/metodoForEach.js
+++ app/metodoForEach.js
@@ -3,9 +3,10 @@ const elementoParaInserirLivros = document.getElementById('livros')
 function exibirOsLivrosNaTela(listaDeLivros) {
     elementoParaInserirLivros.innerHTML = ''
     listaDeLivros.forEach(livro => {
+        let disponibilidade = livro.quantidade > 0 ? 'livro__imagens' : 'livro__imagens indisponivel' 
         elementoParaInserirLivros.innerHTML += `
         <div class="livro">
-        <img class="livro__imagens" src="${livro.imagem}" alt="${livro.alt}"/>
+        <img class="${disponibilidade}" src="${livro.imagem}" alt="${livro.alt}"/>
         <h2 class="livro__titulo">
         ${livro.titulo}
         </h2>
